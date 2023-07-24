264ebb6961072177cd9b46e43a7d934c5cc0f77c
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-07-24 23:30:07 +0300

    Added the login and register button

--- easymedic/App.js
+++ easymedic/App.js
@@ -7,6 +7,22 @@ export default function App() {
   const {height,width} = Dimensions.get("window")
   return (
     <View style={styles.container}>
+      <View style={StyleSheet.absoluteFill}>
+        <Svg height={height} width={width}>
+          <Image href={require("./assets/login-background.jpg")} 
+          width={width}
+          height={height} 
+          preserveAspectRatio='xMidyMid slice'/>
+        </Svg>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>REGISTER</Text>
+        </View>
+      </View>
     </View>
   );
 }
--- easymedic/styles.js
+++ easymedic/styles.js
@@ -2,7 +2,27 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      justifyContent: "flex-end"
     },
+    buttonContainer:{
+        marginBottom:100
+    },
+    button:{
+        backgroundColor: "green",
+        alignSelf:"center",
+        justifyContent:"center",
+        width:300,
+        borderRadius: 20,
+        paddingTop:10,
+        paddingBottom:10,
+        marginTop:20,
+        borderWidth:1,
+        borderColor: "white"
+    },
+    buttonText:{
+        alignSelf:"center",
+        color:"#FFF"
+    }
   });
 
 export default styles;  
