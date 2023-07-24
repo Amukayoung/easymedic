91188f7facf5d28e88d8ff600254dce34f8c08a3
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-07-24 22:35:54 +0300

    Extracted styles to styles.js

--- easymedic/App.js
+++ easymedic/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './styles';
 
 export default function App() {
   return (
@@ -10,11 +11,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
